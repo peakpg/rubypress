da3c69ec3a52a86c909685442b866f8e540840f7
Patrick Peak <peakpg@gmail.com>
2013-04-23 13:02:20 -0400

    Final working copy of the project (prior to screen shots)

--- app/controllers/rubypress/pages_controller.rb
+++ app/controllers/rubypress/pages_controller.rb
@@ -3,5 +3,32 @@ require_dependency "rubypress/application_controller"
 
 module Rubypress
   class PagesController < ApplicationController
+    def index
+      @pages = Page.all
+    end
+    
+    def show
+      @page = Page.find(params[:id])
+    end
+    
+    def new
+      @page = Page.new
+    end
+    
+    def create
+      @page = Page.new(page_params)
+      if @page.save
+        redirect_to @page, notice: 'Page was successfully created.' 
+      else
+        render action: "show" 
+      end
+    end
+
+    private 
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def page_params
+      params.require(:page).permit(:name, :content, :path)
+    end
   end
 end
